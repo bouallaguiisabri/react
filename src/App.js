5d732005b2ca1849bbdf2d746af83460574129ae
000112233 <sabribouallagui@gmail.com>
2022-12-20 12:48:56 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Card from './Component/Card.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Card/>
+      </header>
+  
+    </div>
+  );
+}
+
+export default App;
